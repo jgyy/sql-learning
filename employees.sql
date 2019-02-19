ec7c32c456d3c63f6da7a817a0643b092e3a5b87
jgyy <jgyy@github.com>
2019-02-19 23:56:18 +0800

    adding more and more sql files

--- employees.sql
+++ employees.sql
@@ -7,3 +7,25 @@ CREATE TABLE employees (
   current_status VARCHAR(255) NOT NULL DEFAULT 'employed',
   PRIMARY KEY(id)
 );
+
+INSERT INTO employees(first_name, last_name, age)
+VALUES('Dora', 'Smith', 58);
+
+INSERT INTO cats(name, breed, age)
+VALUES ('Ringo', 'Tabby', 4),
+       ('Cindy', 'Maine Coon', 10),
+       ('Dumbledore', 'Maine Coon', 11),
+       ('Egg', 'Persian', 4),
+       ('Misty', 'Tabby', 13),
+       ('George Michael', 'Ragdoll', 9),
+       ('Jackson', 'Sphynx', 7);
+
+INSERT INTO shirts(article, color, shirt_size, last_worn)
+VALUES ('t-shirt', 'white', 'S', 10),
+       ('t-shirt', 'green', 'S', 200),
+       ('polo shirt', 'black', 'M', 10),
+       ('tank top', 'blue', 'S', 50),
+       ('t-shirt', 'pink', 'S', 0),
+       ('polo shirt', 'red', 'M', 5),
+       ('tank top', 'white', 'S', 200),
+       ('tank top', 'blue', 'M', 15);
